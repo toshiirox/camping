dccd02db9afb45d4e45ef51d46309b9d92f97293
Corentin Chateau <toshiirox@gmail.com>
2015-03-20 16:33:20 +0100

    testtest

---
+++ gestionreservation.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+<head>
+    <!-- titre de la page -->
+    <title>Camping Canargot  - H&ocirc;tellerie de Plein-Air - </title>
+
+    <!-- balise m�ta pr�cisant le jeu de caract�re utilis� -->
+    <meta http-equiv="Content-Type" content="text/HTML; charset=utf-8" />
+
+    <!-- Int�gration de la feuille de style -->
+    <link rel="stylesheet" type="text/css" href="style/principal.css" />
+</head>
+
+<body>
+<div id="conteneur">  <!-- le conteneur entoure tout le contenu de la page -->
+    <a href="index.html"><img alt="logo du camping" src="image/logo_camping.jpg" class="gauche" /></a>   <!-- permet de revenir � la page index en cliquant sur le logo du camping  -->
+    <img alt="label qualite" src="image/camping_qualite.jpg" class="droite" />
+
+
+    <div id="entete">  <!-- bandeau en haut de la page -->
+        <p>Bienvenue au camping l'Escargot</p>
+    </div>  <!-- fin du div entete -->
+
+
+    <div id="conteneurmenu">  <!-- menu apparaissant � gauche de la page -->
+        <ul>
+            <li><a href="index.html">Accueil</a></li>
+            <li><a href="le_camping.html">le camping</a></li>
+            <li><a href="activites.html">les activit&eacute;s</a></li>
+            <li><a href="bungalows.html">les bungalows</a></li>
+            <li><a href="tarifs.html">Tarifs</a></li>
+        </ul>
+    </div>  <!-- fin du div conteneurmenu  -->
+
+
+    <div id="contenu">  <!-- contenu de la page -->
+
+    </div> <!-- fin du div contenu -->
+</div> <!-- fin du div conteneur -->
+
+</body>
+</html>
