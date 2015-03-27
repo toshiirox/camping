d39e618664c2545815dbfec134da4d000a9f4105
Corentin Chateau <toshiirox@gmail.com>
2015-03-27 15:45:09 +0100

    testtest

--- core/menu.php
+++ core/menu.php
@@ -1,10 +1,13 @@
 <div id="conteneurmenu">  <!-- menu apparaissant � gauche de la page -->
     <ul>
-        <li><a href="canargot/index.php">Accueil</a></li>
-        <li><a href="canargot/gestionclient.php">Gestion Client</a></li>
-        <li><a href="canargot/le_camping.php">le camping</a></li>
-        <li><a href="canargot/activites.php">les activit&eacute;s</a></li>
-        <li><a href="canargot/bungalows.php">les bungalows</a></li>
-        <li><a href="canargot/tarifs.php">Tarifs</a></li>
+        <li><a href="index.php">Accueil</a></li>
+        <li><a href="gestionclient.php">Clients</a></li>
+        <li><a href="gestionreservation.php">Réservation</a></li>
+        <li><a href="le_camping.php">le camping</a></li>
+        <li><a href="activites.php">les activit&eacute;s</a></li>
+        <li><a href="bungalows.php">les bungalows</a></li>
+        <li><a href="tarifs.php">Tarifs</a></li>
+
+
     </ul>
 </div>
--- gestionreservation.php
+++ gestionreservation.php
@@ -1,9 +1,48 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php session_start();
+require 'class/autoload.php';
+
+if (isset($_POST['Insertion'])) {
+
+
+// On commence par récupérer les champs
+    if (isset($_POST['nom'])) $nom = $_POST['nom'];
+    else      $nom = "";
+
+    if (isset($_POST['prenom'])) $prenom = $_POST['prenom'];
+    else      $prenom = "";
+
+    if (isset($_POST['email'])) $email = $_POST['email'];
+    else      $email = "";
+
+    if (isset($_POST['adresse'])) $adresse = $_POST['adresse'];
+    else      $adresse = "";
+
+    if (isset($_POST['tel'])) $tel = $_POST['tel'];
+    else      $tel = "";
+
+
+// On vérifie si les champs sont vides
+    if (empty($nom) OR empty($prenom) OR empty($email) OR empty($adresse) OR empty($tel)) {
+    } // Aucun champ n'est vide, on peut enregistrer dans la table
+    else {
+        // connexion à la base
+        $db = DBFactory::getMysqlConnexionWithPDO();
+// sélection de la base
+
+        // on écrit la requête sql
+        $_SESSION['nb'] = $db->exec("INSERT INTO CLIENT(nom, prenom, email, adresse, telephone) VALUES('$nom','$prenom','$email','$adresse','$tel');");
+
+        // on affiche le résultat pour le visiteur
+
+    }
+
+}
+?>
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
 <head>
     <!-- titre de la page -->
-    <title>Camping Canargot - H&ocirc;tellerie de Plein-Air - </title>
+    <title>Camping Canargot - H&ocirc;tellerie de Plein-Air - Accueil</title>
 
     <!-- balise m�ta pr�cisant le jeu de caract�re utilis� -->
     <meta http-equiv="Content-Type" content="text/HTML; charset=utf-8"/>
@@ -13,8 +52,6 @@
 </head>
 
 <body>
-<div id="conteneurmenu">  <!-- menu apparaissant � gauche de la page -->
-    <?php include("core/menu.php"); ?>
 <div id="conteneur">  <!-- le conteneur entoure tout le contenu de la page -->
     <a href="index.php"><img alt="logo du camping" src="image/logo_camping.jpg" class="gauche"/></a>
     <!-- permet de revenir � la page index en cliquant sur le logo du camping  -->
@@ -25,16 +62,28 @@
         <p>Bienvenue au camping l'Escargot</p>
     </div>
     <!-- fin du div entete -->
-
-
-    
+    <?php include("core/menu.php"); ?>
     <!-- fin du div conteneurmenu  -->
 
 
     <div id="contenu">  <!-- contenu de la page -->
-		<form>
-		
-		</form>
+        <h1>Gestion des Réservations</h1>
+
+        <form action="gestionclient.php" method="post">
+            <p>Nom : <input type="text" name="nom" placeholder="Nom" required/></p>
+
+            <p>Prénom : <input type="text" name="prenom" placeholder="Prénom" required/></p>
+
+            <p>Email : <input type="text" name="email" placeholder="example@exe.com" required/></p>
+
+            <p>Adresse : <input type="integer" name="adresse" placeholder="Adresse" required/></p>
+
+            <p>Téléphone : <input type="text" name="tel" placeholder="0605459547" required/></p>
+
+
+            <p><input type="submit" value="Insertion"  name="Insertion"></p>
+        </form>
+
     </div>
     <!-- fin du div contenu -->
 </div>
